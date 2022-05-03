37559addd5665ae0b12082aa0542656b1b50352c
Coda Hale <coda.hale@gmail.com>
2022-05-02 20:36:01 -0600

    perf(benches): re-org the benchmarks a little

--- README.md
+++ README.md
@@ -4,36 +4,60 @@ A Rust implementation of the Cyclist mode of permutation-based cryptography.
 
 Includes Xoodyak and several Keccak-_p_ based constructions.
 
-Hashing performance:
+## Benchmarks
+
+### Hashing
+
+Hashing 100KiB blocks:
+
+```text
+hash/xoodyak            time:   [462.84 us 463.60 us 464.57 us]                         
+                        thrpt:  [210.21 MiB/s 210.65 MiB/s 211.00 MiB/s]
+hash/keccak             time:   [346.01 us 346.32 us 346.72 us]                        
+                        thrpt:  [281.65 MiB/s 281.99 MiB/s 282.24 MiB/s]
+hash/sha256             time:   [327.24 us 327.40 us 327.56 us]                        
+                        thrpt:  [298.13 MiB/s 298.28 MiB/s 298.42 MiB/s]
+hash/sha512             time:   [187.88 us 188.00 us 188.20 us]                        
+                        thrpt:  [518.91 MiB/s 519.43 MiB/s 519.78 MiB/s]
+hash/m14                time:   [111.97 us 112.12 us 112.25 us]                     
+                        thrpt:  [870.01 MiB/s 870.96 MiB/s 872.18 MiB/s]
+hash/k12                time:   [79.161 us 79.182 us 79.207 us]                     
+                        thrpt:  [1.2040 GiB/s 1.2044 GiB/s 1.2047 GiB/s]
 
 ```
-hash/k12                time:   [778.60 us 779.48 us 780.71 us]                     
-                        thrpt:  [1.2509 GiB/s 1.2528 GiB/s 1.2543 GiB/s]
-hash/m14                time:   [1.1077 ms 1.1085 ms 1.1096 ms]                      
-                        thrpt:  [901.20 MiB/s 902.12 MiB/s 902.79 MiB/s]
-hash/keccak             time:   [3.4210 ms 3.4225 ms 3.4243 ms]                         
-                        thrpt:  [292.03 MiB/s 292.18 MiB/s 292.31 MiB/s]
-hash/sha256             time:   [3.3442 ms 3.3451 ms 3.3460 ms]                         
-                        thrpt:  [298.86 MiB/s 298.95 MiB/s 299.02 MiB/s]
-hash/xoodyak            time:   [4.7306 ms 4.7366 ms 4.7451 ms]                          
-                        thrpt:  [210.74 MiB/s 211.12 MiB/s 211.39 MiB/s]
+
+### Authenticated Encryption
+
+Sealing 100KiB blocks:
+
+```text
+aead/aes-256-gcm        time:   [720.64 us 720.92 us 721.27 us]                             
+                        thrpt:  [135.39 MiB/s 135.46 MiB/s 135.51 MiB/s]
+aead/aes-128-gcm        time:   [580.21 us 580.72 us 581.44 us]                             
+                        thrpt:  [167.96 MiB/s 168.17 MiB/s 168.31 MiB/s]
+aead/chacha20poly1305   time:   [400.60 us 401.11 us 402.09 us]                                  
+                        thrpt:  [242.87 MiB/s 243.46 MiB/s 243.77 MiB/s]
+aead/xoodyak            time:   [322.85 us 323.21 us 323.60 us]                         
+                        thrpt:  [301.78 MiB/s 302.14 MiB/s 302.48 MiB/s]
+aead/keccak             time:   [143.87 us 144.09 us 144.36 us]                        
+                        thrpt:  [676.49 MiB/s 677.76 MiB/s 678.78 MiB/s]
+aead/m14                time:   [88.906 us 89.105 us 89.344 us]                     
+                        thrpt:  [1.0674 GiB/s 1.0703 GiB/s 1.0727 GiB/s]
+aead/k12                time:   [74.312 us 74.390 us 74.487 us]                     
+                        thrpt:  [1.2803 GiB/s 1.2820 GiB/s 1.2833 GiB/s]
 ```
 
-AEAD performance (1MiB block size):
+### Permutations
 
-```
-aead/k12                time:   [760.35 us 762.20 us 764.01 us]                     
-                        thrpt:  [1.2782 GiB/s 1.2812 GiB/s 1.2844 GiB/s]
-aead/m14                time:   [905.23 us 907.17 us 908.83 us]                     
-                        thrpt:  [1.0745 GiB/s 1.0765 GiB/s 1.0788 GiB/s]
-aead/keccak             time:   [1.5874 ms 1.5928 ms 1.5993 ms]                         
-                        thrpt:  [625.28 MiB/s 627.81 MiB/s 629.96 MiB/s]
-aead/xoodyak            time:   [3.2611 ms 3.2635 ms 3.2664 ms]                          
-                        thrpt:  [306.15 MiB/s 306.42 MiB/s 306.65 MiB/s]
-aead/chacha20poly1305   time:   [4.0614 ms 4.0689 ms 4.0764 ms]                                   
-                        thrpt:  [245.32 MiB/s 245.76 MiB/s 246.22 MiB/s]
-aead/aes-128-gcm        time:   [5.9243 ms 5.9292 ms 5.9356 ms]                             
-                        thrpt:  [168.48 MiB/s 168.66 MiB/s 168.80 MiB/s]
-aead/aes-256-gcm        time:   [7.3630 ms 7.3705 ms 7.3805 ms]                             
-                        thrpt:  [135.49 MiB/s 135.68 MiB/s 135.81 MiB/s]
+```text
+permutation/keccak      time:   [236.45 ns 236.76 ns 237.15 ns]                               
+                        thrpt:  [804.28 MiB/s 805.60 MiB/s 806.64 MiB/s]
+permutation/m14         time:   [137.90 ns 137.95 ns 138.01 ns]                            
+                        thrpt:  [1.3496 GiB/s 1.3503 GiB/s 1.3508 GiB/s]
+permutation/k12         time:   [118.23 ns 118.29 ns 118.37 ns]                            
+                        thrpt:  [1.5735 GiB/s 1.5747 GiB/s 1.5755 GiB/s]
+permutation/xoodoo      time:   [66.307 ns 66.326 ns 66.347 ns]                               
+                        thrpt:  [689.96 MiB/s 690.18 MiB/s 690.37 MiB/s]
+permutation/xoofff      time:   [33.168 ns 33.186 ns 33.209 ns]                                
+                        thrpt:  [1.3461 GiB/s 1.3471 GiB/s 1.3478 GiB/s]
 ```
--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -2,7 +2,7 @@ use aead::{Aead, NewAead, Payload};
 use aes_gcm::{Aes128Gcm, Aes256Gcm};
 use chacha20poly1305::ChaCha20Poly1305;
 use criterion::{criterion_group, criterion_main, Criterion, Throughput};
-use sha2::{Digest, Sha256};
+use sha2::{Digest, Sha256, Sha512};
 
 use cyclist::keccak::{
     K12Hash, K12Keyed, Keccak, KeccakHash, KeccakKeyed, M14Hash, M14Keyed, K12, M14,
@@ -10,44 +10,52 @@ use cyclist::keccak::{
 use cyclist::xoodoo::{Xoodoo, XoodyakHash, XoodyakKeyed, Xoofff};
 use cyclist::Permutation;
 
-const MB: usize = 1024 * 1024;
+const INPUT: usize = 100 * 1024;
 
 fn hash_benchmarks(c: &mut Criterion) {
     let mut hashing = c.benchmark_group("hash");
-    hashing.throughput(Throughput::Bytes(MB as u64));
-    hashing.bench_with_input("xoodyak", &[0u8; MB], |b, block| {
+    hashing.throughput(Throughput::Bytes(INPUT as u64));
+
+    hashing.bench_with_input("xoodyak", &[0u8; INPUT], |b, block| {
         b.iter(|| {
             let mut st = XoodyakHash::default();
             st.absorb(block);
             st.squeeze(32)
         })
     });
-    hashing.bench_with_input("keccak", &[0u8; MB], |b, block| {
+    hashing.bench_with_input("keccak", &[0u8; INPUT], |b, block| {
         b.iter(|| {
             let mut st = KeccakHash::default();
             st.absorb(block);
             st.squeeze(32)
         })
     });
-    hashing.bench_with_input("k12", &[0u8; MB], |b, block| {
+    hashing.bench_with_input("sha256", &[0u8; INPUT], |b, block| {
         b.iter(|| {
-            let mut st = K12Hash::default();
-            st.absorb(block);
-            st.squeeze(32)
+            let mut digest = Sha256::default();
+            digest.update(block);
+            digest.finalize()
         })
     });
-    hashing.bench_with_input("m14", &[0u8; MB], |b, block| {
+    hashing.bench_with_input("sha512", &[0u8; INPUT], |b, block| {
+        b.iter(|| {
+            let mut digest = Sha512::default();
+            digest.update(block);
+            digest.finalize()
+        })
+    });
+    hashing.bench_with_input("m14", &[0u8; INPUT], |b, block| {
         b.iter(|| {
             let mut st = M14Hash::default();
             st.absorb(block);
             st.squeeze(32)
         })
     });
-    hashing.bench_with_input("sha256", &[0u8; MB], |b, block| {
+    hashing.bench_with_input("k12", &[0u8; INPUT], |b, block| {
         b.iter(|| {
-            let mut digest = Sha256::default();
-            digest.update(block);
-            digest.finalize()
+            let mut st = K12Hash::default();
+            st.absorb(block);
+            st.squeeze(32)
         })
     });
     hashing.finish();
@@ -55,60 +63,8 @@ fn hash_benchmarks(c: &mut Criterion) {
 
 fn aead_benchmarks(c: &mut Criterion) {
     let mut aead = c.benchmark_group("aead");
-    aead.throughput(Throughput::Bytes(MB as u64));
-    aead.bench_with_input("xoodyak", &[0u8; MB], |b, block| {
-        b.iter(|| {
-            let mut st = XoodyakKeyed::new(&[0u8; 32], None, None, None);
-            st.seal(block)
-        })
-    });
-    aead.bench_with_input("keccak", &[0u8; MB], |b, block| {
-        b.iter(|| {
-            let mut st = KeccakKeyed::new(&[0u8; 32], None, None, None);
-            st.seal(block)
-        })
-    });
-    aead.bench_with_input("k12", &[0u8; MB], |b, block| {
-        b.iter(|| {
-            let mut st = K12Keyed::new(&[0u8; 32], None, None, None);
-            st.seal(block)
-        })
-    });
-    aead.bench_with_input("m14", &[0u8; MB], |b, block| {
-        b.iter(|| {
-            let mut st = M14Keyed::new(&[0u8; 32], None, None, None);
-            st.seal(block)
-        })
-    });
-    aead.bench_with_input("chacha20poly1305", &[0u8; MB], |b, block| {
-        let k = [7u8; 32];
-        let n = [8u8; 12];
-        b.iter(|| {
-            let chacha = ChaCha20Poly1305::new(&k.into());
-            chacha.encrypt(
-                &n.into(),
-                Payload {
-                    msg: block,
-                    aad: &[],
-                },
-            )
-        })
-    });
-    aead.bench_with_input("aes-128-gcm", &[0u8; MB], |b, block| {
-        let k = [7u8; 16];
-        let n = [8u8; 12];
-        b.iter(|| {
-            let aes = Aes128Gcm::new(&k.into());
-            aes.encrypt(
-                &n.into(),
-                Payload {
-                    msg: block,
-                    aad: &[],
-                },
-            )
-        })
-    });
-    aead.bench_with_input("aes-256-gcm", &[0u8; MB], |b, block| {
+    aead.throughput(Throughput::Bytes(INPUT as u64));
+    aead.bench_with_input("aes-256-gcm", &[0u8; INPUT], |b, block| {
         let k = [7u8; 32];
         let n = [8u8; 12];
         b.iter(|| {
@@ -122,23 +78,64 @@ fn aead_benchmarks(c: &mut Criterion) {
             )
         })
     });
+    aead.bench_with_input("aes-128-gcm", &[0u8; INPUT], |b, block| {
+        let k = [7u8; 16];
+        let n = [8u8; 12];
+        b.iter(|| {
+            let aes = Aes128Gcm::new(&k.into());
+            aes.encrypt(
+                &n.into(),
+                Payload {
+                    msg: block,
+                    aad: &[],
+                },
+            )
+        })
+    });
+    aead.bench_with_input("chacha20poly1305", &[0u8; INPUT], |b, block| {
+        let k = [7u8; 32];
+        let n = [8u8; 12];
+        b.iter(|| {
+            let chacha = ChaCha20Poly1305::new(&k.into());
+            chacha.encrypt(
+                &n.into(),
+                Payload {
+                    msg: block,
+                    aad: &[],
+                },
+            )
+        })
+    });
+    aead.bench_with_input("xoodyak", &[0u8; INPUT], |b, block| {
+        b.iter(|| {
+            let mut st = XoodyakKeyed::new(&[0u8; 32], None, None, None);
+            st.seal(block)
+        })
+    });
+    aead.bench_with_input("keccak", &[0u8; INPUT], |b, block| {
+        b.iter(|| {
+            let mut st = KeccakKeyed::new(&[0u8; 32], None, None, None);
+            st.seal(block)
+        })
+    });
+    aead.bench_with_input("m14", &[0u8; INPUT], |b, block| {
+        b.iter(|| {
+            let mut st = M14Keyed::new(&[0u8; 32], None, None, None);
+            st.seal(block)
+        })
+    });
+    aead.bench_with_input("k12", &[0u8; INPUT], |b, block| {
+        b.iter(|| {
+            let mut st = K12Keyed::new(&[0u8; 32], None, None, None);
+            st.seal(block)
+        })
+    });
     aead.finish();
 }
 
 fn permutation_benchmarks(c: &mut Criterion) {
     let mut g = c.benchmark_group("permutation");
-    g.bench_function("xoodoo", |b| {
-        let mut state = Xoodoo::new_state();
-        b.iter(|| {
-            Xoodoo::permute(&mut state);
-        })
-    });
-    g.bench_function("xoofff", |b| {
-        let mut state = Xoofff::new_state();
-        b.iter(|| {
-            Xoofff::permute(&mut state);
-        })
-    });
+    g.throughput(Throughput::Bytes(200));
     g.bench_function("keccak", |b| {
         let mut state = Keccak::new_state();
         b.iter(|| {
@@ -155,6 +152,19 @@ fn permutation_benchmarks(c: &mut Criterion) {
         let mut state = K12::new_state();
         b.iter(|| {
             K12::permute(&mut state);
+        })
+    });
+    g.throughput(Throughput::Bytes(48));
+    g.bench_function("xoodoo", |b| {
+        let mut state = Xoodoo::new_state();
+        b.iter(|| {
+            Xoodoo::permute(&mut state);
+        })
+    });
+    g.bench_function("xoofff", |b| {
+        let mut state = Xoofff::new_state();
+        b.iter(|| {
+            Xoofff::permute(&mut state);
         })
     });
     g.finish();
