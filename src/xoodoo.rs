c38abb0acb9263766c29d884c86c94546bdfd829
Coda Hale <coda.hale@gmail.com>
2022-05-07 16:27:40 -0600

    refactor(xoodoo): use xoodoo-p crate

--- src/xoodoo.rs
+++ src/xoodoo.rs
@@ -61,124 +61,19 @@ impl<const R: usize> Permutation<48> for XoodooP<R> {
         let mut st = [0u32; 12];
         LittleEndian::read_u32_into(&self.0, &mut st);
 
-        // Load lanes into registers.
-        let mut st00 = st[0];
-        let mut st01 = st[1];
-        let mut st02 = st[2];
-        let mut st03 = st[3];
-        let mut st04 = st[4];
-        let mut st05 = st[5];
-        let mut st06 = st[6];
-        let mut st07 = st[7];
-        let mut st08 = st[8];
-        let mut st09 = st[9];
-        let mut st10 = st[10];
-        let mut st11 = st[11];
-
-        // Perform last R rounds.
-        for &round_key in &ROUND_KEYS[MAX_ROUNDS - R..MAX_ROUNDS] {
-            let p0 = st00 ^ st04 ^ st08;
-            let p1 = st01 ^ st05 ^ st09;
-            let p2 = st02 ^ st06 ^ st10;
-            let p3 = st03 ^ st07 ^ st11;
-
-            let e0 = p3.rotate_left(5) ^ p3.rotate_left(14);
-            let e1 = p0.rotate_left(5) ^ p0.rotate_left(14);
-            let e2 = p1.rotate_left(5) ^ p1.rotate_left(14);
-            let e3 = p2.rotate_left(5) ^ p2.rotate_left(14);
-
-            let tmp0 = e0 ^ st00 ^ round_key;
-            let tmp1 = e1 ^ st01;
-            let tmp2 = e2 ^ st02;
-            let tmp3 = e3 ^ st03;
-            let tmp4 = e3 ^ st07;
-            let tmp5 = e0 ^ st04;
-            let tmp6 = e1 ^ st05;
-            let tmp7 = e2 ^ st06;
-            let tmp8 = (e0 ^ st08).rotate_left(11);
-            let tmp9 = (e1 ^ st09).rotate_left(11);
-            let tmp10 = (e2 ^ st10).rotate_left(11);
-            let tmp11 = (e3 ^ st11).rotate_left(11);
-
-            st00 = (!tmp4 & tmp8) ^ tmp0;
-            st01 = (!tmp5 & tmp9) ^ tmp1;
-            st02 = (!tmp6 & tmp10) ^ tmp2;
-            st03 = (!tmp7 & tmp11) ^ tmp3;
-
-            st04 = ((!tmp8 & tmp0) ^ tmp4).rotate_left(1);
-            st05 = ((!tmp9 & tmp1) ^ tmp5).rotate_left(1);
-            st06 = ((!tmp10 & tmp2) ^ tmp6).rotate_left(1);
-            st07 = ((!tmp11 & tmp3) ^ tmp7).rotate_left(1);
-
-            st08 = ((!tmp2 & tmp6) ^ tmp10).rotate_left(8);
-            st09 = ((!tmp3 & tmp7) ^ tmp11).rotate_left(8);
-            st10 = ((!tmp0 & tmp4) ^ tmp8).rotate_left(8);
-            st11 = ((!tmp1 & tmp5) ^ tmp9).rotate_left(8);
-        }
-
-        // Load registers into lanes.
-        st[0] = st00;
-        st[1] = st01;
-        st[2] = st02;
-        st[3] = st03;
-        st[4] = st04;
-        st[5] = st05;
-        st[6] = st06;
-        st[7] = st07;
-        st[8] = st08;
-        st[9] = st09;
-        st[10] = st10;
-        st[11] = st11;
+        // Perform the pemutation.
+        xoodoo_p::xoodoo::<R>(&mut st);
 
         // Load lanes into state.
         LittleEndian::write_u32_into(&st, &mut self.0);
     }
 }
 
-const MAX_ROUNDS: usize = 12;
-
-const ROUND_KEYS: [u32; MAX_ROUNDS] = [
-    0x00000058, 0x00000038, 0x000003C0, 0x000000D0, 0x00000120, 0x00000014, 0x00000060, 0x0000002C,
-    0x00000380, 0x000000F0, 0x000001A0, 0x00000012,
-];
-
 #[cfg(test)]
 mod tests {
     use crate::Cyclist;
 
     use super::*;
-
-    #[test]
-    fn xoodoo_kat() {
-        // test vector produced by XKCP rev 2a8d2311a830ab3037f8c7ef2511e5c7cc032127
-        let mut state = Xoodoo::default();
-        state.permute();
-        assert_eq!(
-            state.as_ref(),
-            &[
-                0x8d, 0xd8, 0xd5, 0x89, 0xbf, 0xfc, 0x63, 0xa9, 0x19, 0x2d, 0x23, 0x1b, 0x14, 0xa0,
-                0xa5, 0xff, 0x06, 0x81, 0xb1, 0x36, 0xfe, 0xc1, 0xc7, 0xaf, 0xbe, 0x7c, 0xe5, 0xae,
-                0xbd, 0x40, 0x75, 0xa7, 0x70, 0xe8, 0x86, 0x2e, 0xc9, 0xb7, 0xf5, 0xfe, 0xf2, 0xad,
-                0x4f, 0x8b, 0x62, 0x40, 0x4f, 0x5e,
-            ]
-        );
-    }
-
-    #[test]
-    fn xoodoo6_kat() {
-        // test vector produced by XKCP rev 2a8d2311a830ab3037f8c7ef2511e5c7cc032127
-        let mut state = Xoodoo6::default();
-        state.permute();
-        assert_eq!(
-            state.as_ref(),
-            &[
-                0xa3, 0xce, 0xc9, 0x28, 0x60, 0x4f, 0x20, 0xad, 0xd6, 0xd0, 0xc3, 0x2e, 0xc5, 0xc7,
-                0x50, 0xf0, 0x25, 0x12, 0xdc, 0x08, 0x04, 0x23, 0x99, 0x61, 0x2d, 0x40, 0x0d, 0x9e,
-                0x9b, 0x9b, 0xd5, 0x42, 0xfc, 0x14, 0x61, 0x1e, 0x97, 0xb6, 0x6e, 0x18, 0x7f, 0xbc,
-                0xdb, 0x35, 0x4e, 0x10, 0xf9, 0xa1,
-            ]
-        );
-    }
 
     #[test]
     fn supercop_aead_round_3_test_vector() {
