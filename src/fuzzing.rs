7224dba4ebdd214303b1126dfa9793c85eb888c2
Coda Hale <coda.hale@gmail.com>
2022-10-01 11:44:13 -0600

    refactor(fuzzing): basically rewrite it

--- src/fuzzing.rs
+++ src/fuzzing.rs
@@ -1,30 +1,51 @@
 #![cfg(all(test, feature = "std", feature = "xoodyak"))]
 
+use std::iter;
+
 use proptest::collection::vec;
 use proptest::prelude::*;
 
 use crate::xoodyak::{XoodyakHash, XoodyakKeyed};
 use crate::Cyclist;
 
+/// An input operation for Cyclist's hash mode.
 #[derive(Clone, Debug, PartialEq)]
 enum HashOp {
     Absorb(Vec<u8>),
     Squeeze(usize),
 }
 
+/// An output from Cyclist's hash mode.
 #[derive(Clone, Debug, PartialEq)]
-enum KeyedOp {
-    Absorb(Vec<u8>),
-    Squeeze(usize),
-    Crypt(Vec<u8>),
-    Ratchet,
+enum HashOutput {
+    Squeezed(Vec<u8>),
 }
 
+/// A transcript of input operations for Cyclist's hash mode.
 #[derive(Clone, Debug, PartialEq)]
 struct HashTranscript {
     ops: Vec<HashOp>,
 }
 
+/// An input operation for Cyclist's keyed mode.
+#[derive(Clone, Debug, PartialEq)]
+enum KeyedOp {
+    Absorb(Vec<u8>),
+    Squeeze(usize),
+    Encrypt(Vec<u8>),
+    Decrypt(Vec<u8>),
+    Ratchet,
+}
+
+/// An output from Cyclist's keyed mode.
+#[derive(Clone, Debug, PartialEq)]
+enum KeyedOutput {
+    Squeezed(Vec<u8>),
+    Encrypted(Vec<u8>),
+    Decrypted(Vec<u8>),
+}
+
+/// A transcript of input operations for Cyclist's keyed mode, plus shared key, nonce, and counter.
 #[derive(Clone, Debug, PartialEq)]
 struct KeyedTranscript {
     key: Vec<u8>,
@@ -33,91 +54,77 @@ struct KeyedTranscript {
     ops: Vec<KeyedOp>,
 }
 
-fn apply_hash_transcript(transcript: HashTranscript) -> Vec<u8> {
+/// Apply the transcript's operations to Xoodyak in hash mode, plus a final `Squeeze(16)` to
+/// establish the duplex's final state, and return the duplex's outputs.
+fn apply_hash_transcript(transcript: &HashTranscript) -> Vec<HashOutput> {
     let mut hash = XoodyakHash::default();
-    let mut squeezed = Vec::new();
+    transcript
+        .ops
+        .iter()
+        .chain(iter::once(&HashOp::Squeeze(16)))
+        .flat_map(|op| match op {
+            HashOp::Absorb(data) => {
+                hash.absorb(data);
+                None
+            }
+            HashOp::Squeeze(n) => Some(HashOutput::Squeezed(hash.squeeze(*n))),
+        })
+        .collect()
+}
+
+/// Apply the transcript's operations to Xoodyak in keyed mode, plus a final `Squeeze(16)` to
+/// establish the duplex's final state, and return the duplex's outputs.
+fn apply_keyed_transcript(transcript: &KeyedTranscript) -> Vec<KeyedOutput> {
+    let mut keyed = XoodyakKeyed::new(&transcript.key, &transcript.nonce, &transcript.counter);
+    transcript
+        .ops
+        .iter()
+        .chain(iter::once(&KeyedOp::Squeeze(16)))
+        .flat_map(|op| match op {
+            KeyedOp::Absorb(data) => {
+                keyed.absorb(data);
+                None
+            }
+            KeyedOp::Squeeze(n) => Some(KeyedOutput::Squeezed(keyed.squeeze(*n))),
+            KeyedOp::Encrypt(data) => Some(KeyedOutput::Encrypted(keyed.encrypt(data))),
+            KeyedOp::Decrypt(data) => Some(KeyedOutput::Decrypted(keyed.decrypt(data))),
+            KeyedOp::Ratchet => {
+                keyed.ratchet();
+                None
+            }
+        })
+        .collect()
+}
+
+/// Apply the transcript's operations to two duplexes--`outbound` and `inbound`--checking that both
+/// duplexes can correctly encrypt and decrypt each other's outputs and remain synchronized.
+fn check_keyed_transcript_symmetry(transcript: &KeyedTranscript) {
+    let mut outbound = XoodyakKeyed::new(&transcript.key, &transcript.nonce, &transcript.counter);
+    let mut inbound = XoodyakKeyed::new(&transcript.key, &transcript.nonce, &transcript.counter);
 
     for op in &transcript.ops {
         match op {
-            HashOp::Absorb(data) => {
-                hash.absorb(data);
+            KeyedOp::Absorb(data) => {
+                outbound.absorb(data);
+                inbound.absorb(data);
             }
-            HashOp::Squeeze(n) => {
-                let out = hash.squeeze(*n);
-                squeezed.extend_from_slice(&out);
+            KeyedOp::Squeeze(n) => {
+                assert_eq!(outbound.squeeze(*n), inbound.squeeze(*n));
+            }
+            KeyedOp::Encrypt(plaintext) => {
+                assert_eq!(plaintext, &inbound.decrypt(&outbound.encrypt(plaintext)));
+            }
+            KeyedOp::Decrypt(ciphertext) => {
+                assert_eq!(ciphertext, &inbound.encrypt(&outbound.decrypt(ciphertext)));
+            }
+            KeyedOp::Ratchet => {
+                outbound.ratchet();
+                inbound.ratchet();
             }
         }
     }
 
-    squeezed.extend_from_slice(&hash.squeeze(16));
-    squeezed
-}
-
-fn apply_keyed_transcript(transcript: KeyedTranscript) {
-    let mut outbound_squeezed = Vec::new();
-    let mut inbound_squeezed = Vec::new();
-
-    let inbound_ops = {
-        let mut outbound =
-            XoodyakKeyed::new(&transcript.key, &transcript.nonce, &transcript.counter);
-        let ops = transcript
-            .ops
-            .iter()
-            .map(|op| match op {
-                KeyedOp::Absorb(data) => {
-                    outbound.absorb(data);
-                    KeyedOp::Absorb(data.to_vec())
-                }
-                KeyedOp::Squeeze(n) => {
-                    let out = outbound.squeeze(*n);
-                    outbound_squeezed.extend_from_slice(&out);
-                    KeyedOp::Squeeze(*n)
-                }
-                KeyedOp::Crypt(plaintext) => {
-                    let ciphertext = outbound.encrypt(plaintext);
-                    KeyedOp::Crypt(ciphertext)
-                }
-                KeyedOp::Ratchet => {
-                    outbound.ratchet();
-                    KeyedOp::Ratchet
-                }
-            })
-            .collect::<Vec<KeyedOp>>();
-        outbound_squeezed.extend_from_slice(&outbound.squeeze(16));
-        ops
-    };
-
-    let outbound_ops = {
-        let mut inbound =
-            XoodyakKeyed::new(&transcript.key, &transcript.nonce, &transcript.counter);
-        let ops = inbound_ops
-            .into_iter()
-            .map(|op| match op {
-                KeyedOp::Absorb(data) => {
-                    inbound.absorb(&data);
-                    KeyedOp::Absorb(data)
-                }
-                KeyedOp::Squeeze(n) => {
-                    let out = inbound.squeeze(n);
-                    inbound_squeezed.extend_from_slice(&out);
-                    KeyedOp::Squeeze(n)
-                }
-                KeyedOp::Crypt(ciphertext) => {
-                    let plaintext = inbound.decrypt(&ciphertext);
-                    KeyedOp::Crypt(plaintext)
-                }
-                KeyedOp::Ratchet => {
-                    inbound.ratchet();
-                    KeyedOp::Ratchet
-                }
-            })
-            .collect::<Vec<KeyedOp>>();
-        inbound_squeezed.extend_from_slice(&inbound.squeeze(16));
-        ops
-    };
-
-    assert_eq!(outbound_squeezed, inbound_squeezed);
-    assert_eq!(transcript.ops, outbound_ops);
+    assert_eq!(outbound.squeeze(16), inbound.squeeze(16));
 }
 
 fn arb_data() -> impl Strategy<Value = Vec<u8>> {
@@ -144,7 +151,8 @@ fn arb_keyed_op() -> impl Strategy<Value = KeyedOp> {
     prop_oneof![
         arb_data().prop_map(KeyedOp::Absorb),
         (1usize..256).prop_map(KeyedOp::Squeeze),
-        arb_data().prop_map(KeyedOp::Crypt),
+        arb_data().prop_map(KeyedOp::Encrypt),
+        arb_data().prop_map(KeyedOp::Decrypt),
         Just(KeyedOp::Ratchet),
     ]
 }
@@ -164,11 +172,22 @@ prop_compose! {
 proptest! {
     #[test]
     fn hash_transcript_consistency(t0 in arb_hash_transcript(), t1 in arb_hash_transcript()) {
-        let eq = t0 == t1;
-        let out0 = apply_hash_transcript(t0);
-        let out1 = apply_hash_transcript(t1);
+        let out0 = apply_hash_transcript(&t0);
+        let out1 = apply_hash_transcript(&t1);
 
-        if eq {
+        if t0 == t1 {
+            assert_eq!(out0, out1);
+        } else  {
+            assert_ne!(out0, out1);
+        }
+    }
+
+    #[test]
+    fn keyed_transcript_consistency(t0 in arb_keyed_transcript(), t1 in arb_keyed_transcript()) {
+        let out0 = apply_keyed_transcript(&t0);
+        let out1 = apply_keyed_transcript(&t1);
+
+        if t0 == t1 {
             assert_eq!(out0, out1);
         } else  {
             assert_ne!(out0, out1);
@@ -177,6 +196,6 @@ proptest! {
 
     #[test]
     fn keyed_transcript_symmetry(t in arb_keyed_transcript()) {
-        apply_keyed_transcript(t);
+        check_keyed_transcript_symmetry(&t);
     }
 }
